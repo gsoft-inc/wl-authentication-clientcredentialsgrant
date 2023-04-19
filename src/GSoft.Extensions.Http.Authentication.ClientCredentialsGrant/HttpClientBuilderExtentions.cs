427459e3dafd2dc9cc2b28d06f1b8218543d327c
Guillaume Caya-Letourneau <guillaume.caya@gsoft.com>
2023-04-19 10:23:38 -0400

    Added base public contract

---
+++ src/GSoft.Extensions.Http.Authentication.ClientCredentialsGrant/HttpClientBuilderExtentions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace GSoft.Extensions.Http.Authentication.ClientCredentialsGrant;
+
+public static class HttpClientBuilderExtentions
+{
+    public static IHttpClientBuilder AddClientCredentials(this IHttpClientBuilder builder, Action<ClientCredentialsOptions>? configure = null)
+    {
+        builder.Services.AddOptions<ClientCredentialsOptions>(builder.Name)
+            .BindConfiguration($"{ClientCredentialsOptions.BaseSectionName}:{builder.Name}");
+
+        if (configure != null)
+        {
+            builder.Services.Configure(builder.Name, configure);
+        }
+
+        return builder;
+    }
+}
