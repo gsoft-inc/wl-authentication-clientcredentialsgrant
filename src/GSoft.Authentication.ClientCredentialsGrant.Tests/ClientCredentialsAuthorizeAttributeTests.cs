31bf746dc0fcc51f88480345a09d6b8600a7623f
Guillaume Caya-Letourneau <guillaume.caya@gsoft.com>
2023-05-08 23:20:24 -0400

    Made attribute tests a bit more future proof

--- src/GSoft.Authentication.ClientCredentialsGrant.Tests/ClientCredentialsAuthorizeAttributeTests.cs
+++ src/GSoft.Authentication.ClientCredentialsGrant.Tests/ClientCredentialsAuthorizeAttributeTests.cs
@@ -1,4 +1,4 @@
-﻿using GSoft.AspNetCore.Authentication.ClientCredentialsGrant;
+﻿using System.Collections;
 using Microsoft.AspNetCore.Authorization;
 
 namespace GSoft.Authentication.ClientCredentialsGrant.Tests;
@@ -6,13 +6,31 @@ namespace GSoft.Authentication.ClientCredentialsGrant.Tests;
 public class ClientCredentialsAuthorizeAttributeTests
 {
     [Theory]
-    [InlineData(ClientCredentialsScope.Read, ClientCredentialsDefaults.AuthorizationReadPolicy)]
-    [InlineData(ClientCredentialsScope.Write, ClientCredentialsDefaults.AuthorizationWritePolicy)]
-    [InlineData(ClientCredentialsScope.Admin, ClientCredentialsDefaults.AuthorizationAdminPolicy)]
-    public void Given_Valid_Scope_When_Create_Then_Map_To_Proper_Policy_Name(ClientCredentialsScope scope, string expectedPolicy)
+    [ClassData(typeof(ClientCredentialsScopeData))]
+    public void Given_All_Possible_Scopes_When_Create_Then_Mapped_To_Policy(ClientCredentialsScope scope)
     {
         var attribute = new ClientCredentialsAuthorizeAttribute(scope);
-        Assert.Equal(expectedPolicy, attribute.Policy);
         Assert.Equal(scope, attribute.Scope);
+        Assert.NotNull(attribute.Policy);
+    }
+
+    [Fact]
+    public void Given_InValid_Scope_When_Create_Then_Map_To_Proper_Policy_Name()
+    {
+        var scope = (ClientCredentialsScope)999;
+        Assert.Throws<ArgumentException>(() => new ClientCredentialsAuthorizeAttribute(scope));
+    }
+
+    private sealed class ClientCredentialsScopeData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            foreach (var value in Enum.GetValues<ClientCredentialsScope>())
+            {
+                yield return new object[] { value };
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
     }
 }
